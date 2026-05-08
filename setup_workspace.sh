4bdcf70f295959910c77f2db7f75f67f83cb0624
szymon <vatrasar@gmail.com>
2026-05-08 18:32:10 +0200

    adding gemini md file using sh script

--- setup_workspace.sh
+++ setup_workspace.sh
@@ -19,6 +19,7 @@ echo "Pobieranie repozytorium rules..."
 echo "=========================================="
 # Sklonowanie repozytorium rules pod stałą nazwą 'rules'
 git clone "$RULES_REPO_URL" rules
+echo "in folders of this project you can find rules and skills for AI coding agent. your job is help me to adjust this rules and skills." > rules/GEMINI.md
 
 echo "=========================================="
 echo "Pobieranie projektów..."
